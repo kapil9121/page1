ecc8b7141c19b9693b838ca63de4260eed6fb069
kapil9121 <rathodkapil9121@gmail.com>
2024-07-06 13:45:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+    $( "#hi" ).click(function() {
+       $("#alert-div").toggle();
+    });
+   });
+
+   $( document ).ready(function() {
+      $( "#hi" ).click(function() {
+         $("#desc").toggle();
+      });
+     });
